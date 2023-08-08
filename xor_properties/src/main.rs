daffa9c2a1923fc850ca6ed281dc91c986c200dc
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-08-08 17:51:15 -0400

    added xor variables need to convert this into functions.

--- xor_properties/src/main.rs
+++ xor_properties/src/main.rs
@@ -2,21 +2,19 @@ use hex;
 use std::fmt;
 
 fn main() {
-    let key = "a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313";
-    let bin_vec = key_to_bin(key);
-    println!("the binary values are : \n{:?}\n", key_to_bin(key));
-    println!("there ar: {} elements in the vec", bin_vec.len() );
-    let new_bin = bin_vec.clone();
+    let key1 = "a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313";
+    let key2_xor_key1 = "37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e";
 
-    compare_bytes(vec![bin_vec, new_bin]);
-}
+    let key1_bytes = hex::decode(key1).unwrap();
+    println!("the bytes of key1 are: {:?}", key1_bytes);
 
-fn key_to_bin(key: &str) -> Vec<String> {
-    let val = hex::decode(key).unwrap();
-    val.iter().map(|byte| format!("{:08b}", byte)).collect()
-}
+    let key2_xor_key1_bytes = hex::decode(key2_xor_key1).unwrap();
+    println!("the k2 and k1 bytes are: {:?}", key2_xor_key1_bytes);
 
-fn compare_bytes(bin_vecs: Vec<Vec<String>>) {
+    let key2_bytes: Vec<u8> = key1_bytes.iter().zip(key2_xor_key1_bytes.iter()).map(|(k1, k2)| k1 ^ k2).collect();
 
     
+
+    //compare_bytes(vec![bin_vec, new_bin]);
 }
+

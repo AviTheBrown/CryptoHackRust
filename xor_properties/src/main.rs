a31603bee0a58f29c43976ba0308664bef7d1b70
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-08-13 16:47:56 -0400

    refactored.

--- xor_properties/src/main.rs
+++ xor_properties/src/main.rs
@@ -86,60 +86,48 @@ fn main() {
 
     // key1_bytes -> we first decode the hex number and get ther result of the option.
     // pub fn decode<T: AsRef<[u8]>>(data: T) -> Result<Vec<u8>, FromHexError>
-    let key1_bytes = hex::decode(key1).unwrap();
+    //let key1_bytes = hex::decode(key1).unwrap();
 
     // this decodes key2 ^ key1 ( this HASH is the evaluated result of key1 ^ key2 not key2 itselt)
-    let key2_xor_key1_bytes = hex::decode(key2_xor_key1).unwrap();
+    //let key2_xor_key1_bytes = hex::decode(key2_xor_key1).unwrap();
 
     let k2_b = xor_bytes_to_bytes_1(&k1_b, &k2_x_k1_b);
     // inorder to get the byte value for key2
     // we iterate over the bytes of key1 and key1&key2 bytes together using zip using map then collecting the bytes.
-    let key2_bytes: Vec<u8> = key1_bytes
-        .iter()
-        .zip(key2_xor_key1_bytes.iter())
-        .map(|(k1, k2)| k1 ^ k2)
-        .collect();
+ //   let key2_bytes: Vec<u8> = key1_bytes
+   //     .iter()
+     //   .zip(key2_xor_key1_bytes.iter())
+       // .map(|(k1, k2)| k1 ^ k2)
+        //.collect();
 
     let key2_xor_key3 = "c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1";
-    let key2_xor_key3_bytes = hex::decode(key2_xor_key3).unwrap();
+    //let key2_xor_key3_bytes = hex::decode(key2_xor_key3).unwrap();
 
     let k2_k3_b = hash_to_bytes(key2_xor_key3);
     let k3_b = xor_bytes_to_bytes_1(&k2_b, &k2_k3_b);
 
     // the same process as previous
-    let key3_bytes: Vec<u8> = key2_bytes
-        .iter()
-        .zip(key2_xor_key3_bytes.iter())
-        .map(|(k2, k3)| k2 ^ k3)
-        .collect();
 
     let flag_k1_k2_k3 = "04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf";
-    let flag_k1_k2_k3_bytes = hex::decode(flag_k1_k2_k3).unwrap();
 
     let fk1k2k3_b = hash_to_bytes(flag_k1_k2_k3);
 
-    println!("the flag bytes are:{:?}", flag_k1_k2_k3_bytes);
 
     let the_flag_b = xor_bytes_to_bytes(&[&fk1k2k3_b, &k1_b, &k2_b, &k3_b]);
     // this on is a bit differnent as it uses multiple zips to xor all the bytes using the associate propertie
-    let flag_bytes: Vec<u8> = flag_k1_k2_k3_bytes
+    //let flag_bytes: Vec<u8> = flag_k1_k2_k3_bytes
         // clones the byte as it is used on line 41
-        .clone()
+      //  .clone()
         // into_iter() take posession of the object
         // i cpuld have used iter() as well
-        .into_iter()
+        //.into_iter()
         // the zips tie each byte, so that they can be used together for whatever reason.
-        .zip(key1_bytes.iter())
-        .zip(key2_bytes.iter())
-        .zip(key3_bytes.iter())
+        //.zip(key1_bytes.iter())
+        //.zip(key2_bytes.iter())
+        //.zip(key3_bytes.iter())
         // closure using lamda function to xor all the btyes
-        .map(|(((fg, k1), k2), k3)| fg ^ k1 ^ k2 ^ k3)
+        //.map(|(((fg, k1), k2), k3)| fg ^ k1 ^ k2 ^ k3)
         // collects all result of the inital collection (Vec<u8> from hash::decode()) into the new collection (Vec<u8> type annotation)
-        .collect();
+        //.collect();
 
-    println!("the flag bytes are: {:?}", flag_bytes);
-
-    println!("the lenght is : {:?}", key3_bytes.len());
-
-    println!("the string is: {:?}", ascii_to_chars(Bytes(flag_bytes)));
 }
